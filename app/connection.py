ffeef79d425ab40c263c6af694ad44a5797adcdb
Yehuda gavrieli <yehuda8009@gmail.com>
2026-02-05 10:16:25 +0200

    test

--- app/connection.py
+++ app/connection.py
@@ -1,13 +1,14 @@
 from pymongo import MongoClient
 import os
 
+
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
-DATABASE_NAME = "helios_db"
+DB_NAME = "employee_db"
 COLLECTION_NAME = "employees"
 
 client = MongoClient(MONGO_URI)
-db = client[DATABASE_NAME]
-collection = db[COLLECTION_NAME]
+db = client[DB_NAME]
+employees_col = db[COLLECTION_NAME]
 
 
 

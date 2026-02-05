abcef9cb515432c32c5f0570791a193e16d6ef88
Yehuda gavrieli <yehuda8009@gmail.com>
2026-02-05 10:25:06 +0200

    test

--- app/main.py
+++ app/main.py
@@ -4,11 +4,11 @@ import dal
 app = FastAPI()
 
 @app.get("/employees/engineering/high-salary")
-def engineering_high_salary():
+def enginere_high_salary():
     return dal.get_engineering_high_salary_employees()
 
 @app.get("/employees/by-age-and-role")
-def by_age_role():
+def age_and_role():
     return dal.get_employees_by_age_and_role()
 
 @app.get("/employees/top-seniority")
@@ -16,13 +16,13 @@ def top_seniority():
     return dal.get_top_seniority_employees_excluding_hr()
 
 @app.get("/employees/age-or-seniority")
-def age_seniority():
+def age_or_seniority():
     return dal.get_employees_by_age_or_seniority()
 
 @app.get("/employees/managers/excluding-departments")
-def managers_no_sales_marketing():
+def managers_no_excluding_departments():
     return dal.get_managers_excluding_departments()
 
 @app.get("/employees/by-lastname-and-age")
-def lastname_age():
+def lastname_and_age():
     return dal.get_employees_by_lastname_and_age()
